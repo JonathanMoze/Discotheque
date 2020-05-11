9f212c1c08e57d74573c0a9d7cadfeb19e95afa8
Pierre Ramet <ramet_pierre@hotmail.fr>
2020-05-11 15:48:58 +0200

    corrections Eric

--- exemples/EF_Musiciens_WindowsForms_App/WindowsFormsApp2/Form1.cs
+++ exemples/EF_Musiciens_WindowsForms_App/WindowsFormsApp2/Form1.cs
@@ -6,37 +6,49 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
         MusiqueSQLEntities musique;
+
         public Form1()
         {
             InitializeComponent();
             musique = new MusiqueSQLEntities();
+            ChargeLesCompositeurs();
+        }
 
+        private void ChargeLesCompositeurs()
+        {
+            // on récupère tous les compositeurs
             var musiciens = (from m in musique.Musicien
-                             //where m.Nom_Musicien.StartsWith(textBox1.Text)
+                             where m.Oeuvre.Count > 0
                              orderby m.Nom_Musicien
                              select m).ToList();
+            // on crée les objets locaux et on remplit la listbox
             foreach (Musicien m in musiciens)
             {
-                //Console.Write(m.Nom_Musicien + ", ");
                 listBox1.Items.Add(m);
             }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            listBox1.Items.Clear();
-            listBox2.Items.Clear();
+            // on récupère tous les compositeurs satisfaisant le critère de recherche
+            // ToUpper --> pour rester "case insensitive"
             var musiciens = (from m in musique.Musicien
-                             where m.Nom_Musicien.StartsWith(textBox1.Text)
+                             where m.Oeuvre.Count > 0 && m.Nom_Musicien.ToUpper().Contains(textBox1.Text.ToUpper())
                              orderby m.Nom_Musicien
                              select m).ToList();
+            // on réinitialise les deux listBox
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            // on insère dans listBox1 les musiciens récupérés
             foreach (Musicien m in musiciens)
             {
                 listBox1.Items.Add(m);
@@ -47,17 +59,10 @@ namespace WindowsFormsApp2
         {
             listBox2.Items.Clear();
             Musicien m = (Musicien)listBox1.SelectedItem;
-            
             foreach (Oeuvre o in m.Oeuvre)
             {
-                //listBox2.Items.Add(o.Titre_Oeuvre);
-                listBox2.Items.Add(o);
+                listBox2.Items.Add(o.Titre_Oeuvre);
             }
-            /*
-            foreach (Diriger d in x.me.Diriger)
-                foreach (Composition_Disque c in d.Enregistrement.Composition_Disque)
-                    listBox2.Items.Add(c.Disque.Album.Titre_Album);
-            */
         }
     }
 }
