dc73118ed3e1f86159e11843c5b998d4e4274c9c
Priscilla <priscilla.tissot@etu.u-bordeaux.fr>
2020-06-02 20:13:01 +0200

    Phase 5.1 finie et 5.2 en cours

---
+++ projet_code/projet/ListeAbonne.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projet
+{
+    public partial class ListeAbonne : Form
+    {
+        MusiqueSQLEntities musiqueBase;
+        Dictionary<int, DateTime> PersEmpr;
+        public ListeAbonne()
+        {
+            InitializeComponent();
+            musiqueBase = new MusiqueSQLEntities();
+            PersEmpr = new Dictionary<int, DateTime>();
+            ChargeListBoxTitresAlbums();
+        }
+
+        private void ChargeListBoxTitresAlbums()
+        {
+            // on récupère tous les albums
+            var abonnés = (from a in musiqueBase.Abonné
+                           orderby a.Nom_Abonné
+                           select a).ToList();
+
+            var emprunts = (from e in musiqueBase.Emprunter
+                            orderby e.Code_Abonné
+                            select e).ToList();
+            // on initialise la listbox
+            listBox1.Items.Clear();
+            // création des objets locaux et remplissage de la listbox
+            foreach (Abonné a in abonnés)
+            {
+                string nom, prenom;
+                DateTime dateEmprunt = default;
+                //Chercher la date la plus récente
+                foreach (Emprunter e in emprunts)
+                {
+                    if (a.Code_Abonné == e.Code_Abonné && DateTime.Compare(dateEmprunt, (DateTime)e.Date_Emprunt) < 0)
+                    {
+                        dateEmprunt = (DateTime)e.Date_Emprunt;
+                    }
+                }
+                PersEmpr[a.Code_Abonné] = dateEmprunt;
+                // Mettre un nom sans espaces 
+                if (a.Nom_Abonné == null) nom = "Sans nom";
+                else
+                {
+                    string[] v = System.Text.RegularExpressions.Regex.Split(a.Nom_Abonné, "  ");
+                    nom = v[0];
+                }
+                // Mettre un prénom sans espaces
+                if (a.Prénom_Abonné == null) prenom = "Sans prénom";
+                else
+                {
+                    string[] v = System.Text.RegularExpressions.Regex.Split(a.Prénom_Abonné, "  ");
+                    prenom = v[0];
+                }
+                //Ajout dans la listBox selon la présence de date
+                if (dateEmprunt == default)
+                {
+                    listBox1.Items.Add(nom + " " + prenom + "    -    N'a pas encore fait d'emprunt");
+                }
+                else listBox1.Items.Add(nom + " " + prenom + "    -    " + dateEmprunt.ToString());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (KeyValuePair<int, DateTime> n in PersEmpr)
+            {
+                if (DateTime.Compare(n.Value.AddDays(365), DateTime.Now) < 0)
+                {
+                    Abonné abn = new Abonné();
+                    var abonnés = (from a in musiqueBase.Abonné
+                                   where a.Code_Abonné == n.Key
+                                   orderby a.Nom_Abonné
+                                   select a).ToList();
+                    abn = abonnés.First();
+                    musiqueBase.Abonné.Remove(abn);
+                    musiqueBase.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ projet_code/projet/MenuAdmin.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace projet
+{
+    partial class MenuAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.infoAdmin = new System.Windows.Forms.Label();
+            this.aboInactifs = new System.Windows.Forms.Button();
+            this.retardsEmprunts = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // infoAdmin
+            // 
+            this.infoAdmin.AutoSize = true;
+            this.infoAdmin.Location = new System.Drawing.Point(9, 7);
+            this.infoAdmin.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.infoAdmin.Name = "infoAdmin";
+            this.infoAdmin.Size = new System.Drawing.Size(113, 13);
+            this.infoAdmin.TabIndex = 0;
+            this.infoAdmin.Text = "Session Administrateur";
+            // 
+            // aboInactifs
+            // 
+            this.aboInactifs.Location = new System.Drawing.Point(178, 113);
+            this.aboInactifs.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.aboInactifs.Name = "aboInactifs";
+            this.aboInactifs.Size = new System.Drawing.Size(214, 37);
+            this.aboInactifs.TabIndex = 1;
+            this.aboInactifs.Text = "Gérer les abonnés inactifs";
+            this.aboInactifs.UseVisualStyleBackColor = true;
+            this.aboInactifs.Click += new System.EventHandler(this.aboInactifs_Click);
+            // 
+            // retardsEmprunts
+            // 
+            this.retardsEmprunts.Location = new System.Drawing.Point(178, 187);
+            this.retardsEmprunts.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.retardsEmprunts.Name = "retardsEmprunts";
+            this.retardsEmprunts.Size = new System.Drawing.Size(214, 37);
+            this.retardsEmprunts.TabIndex = 2;
+            this.retardsEmprunts.Text = "Gérer les emprunts non rapportés";
+            this.retardsEmprunts.UseVisualStyleBackColor = true;
+            // 
+            // MenuAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.retardsEmprunts);
+            this.Controls.Add(this.aboInactifs);
+            this.Controls.Add(this.infoAdmin);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "MenuAdmin";
+            this.Text = "MenuAdmin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label infoAdmin;
+        private System.Windows.Forms.Button aboInactifs;
+        private System.Windows.Forms.Button retardsEmprunts;
+    }
+}
