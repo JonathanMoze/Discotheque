e89c483edb6bab3a99b5186380794ea0beea8d11
Adélaïde Louis <adelaide.louis@etu.u-bordeaux.fr>
2020-06-04 14:31:37 +0200

    Correction bugs dans Retards et ListAbonne

--- projet_code/projet/projet/ListeAbonne.Designer.cs
+++ projet_code/projet/projet/ListeAbonne.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.labelMessage = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
@@ -37,25 +36,19 @@
             this.titreRetards = new System.Windows.Forms.Label();
             this.labelDate = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // labelMessage
             // 
             this.labelMessage.AutoSize = true;
-            this.labelMessage.Location = new System.Drawing.Point(361, 279);
+            this.labelMessage.Location = new System.Drawing.Point(451, 259);
             this.labelMessage.Name = "labelMessage";
             this.labelMessage.Size = new System.Drawing.Size(13, 13);
             this.labelMessage.TabIndex = 7;
             this.labelMessage.Text = "  ";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 79);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 13);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Nom Prénom ";
             // 
             // button1
             // 
@@ -72,7 +65,7 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(14, 109);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(296, 251);
+            this.listBox1.Size = new System.Drawing.Size(296, 108);
             this.listBox1.TabIndex = 4;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
@@ -126,18 +119,46 @@
             this.label2.TabIndex = 12;
             this.label2.Text = "Sélectionnez un abonné pour voir sa dernière date d\'emprunt";
             // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(15, 259);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(374, 95);
+            this.listBox2.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Abonnés inactifs";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Tous les abonnés";
+            // 
             // ListeAbonne
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.labelDate);
             this.Controls.Add(this.titreRetards);
             this.Controls.Add(this.infoAdmin);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.labelMessage);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.Name = "ListeAbonne";
@@ -150,7 +171,6 @@
         #endregion
 
         private System.Windows.Forms.Label labelMessage;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button2;
@@ -158,5 +178,8 @@
         private System.Windows.Forms.Label titreRetards;
         private System.Windows.Forms.Label labelDate;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
